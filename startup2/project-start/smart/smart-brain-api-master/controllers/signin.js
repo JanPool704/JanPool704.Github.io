6ad975455a165f102031d590e4fa511996466577
JanPool704 <78930612+JanPool704@users.noreply.github.com>
2021-02-28 17:05:04 -0600

    mi app smart

---
+++ startup2/project-start/smart/smart-brain-api-master/controllers/signin.js
@@ -0,0 +1,29 @@
+const handleSignin = (db, bcrypt) => (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json('incorrect form submission');
+  }
+  db.select('email', 'hash').from('login')
+    .where('email', '=', email)
+    .then(data => {
+      console.log('entro a la consulta');
+      const isValid = bcrypt.compareSync(password, data[0].hash);
+      console.log('esta validando el password');
+      if (isValid) {
+        console.log('es valido el password');
+        return db.select('*').from('users')
+          .where('email', '=', email)
+          .then(user => {
+            res.json(user[0])
+          })
+          .catch(err => res.status(400).json('unable to get user'))
+      } else {
+        res.status(400).json('wrong credentials')
+      }
+    })
+    .catch(err => res.status(400).json('wrong credentials catch'))
+}
+
+module.exports = {
+  handleSignin: handleSignin
+}
